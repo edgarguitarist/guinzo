2565e089f960f066e166611a2b7a1f1febda1065
edgarguitarist <edgarguitarist@gmail.com>
2022-02-24 20:49:49 -0500

    fixing style

--- sistema/components/events-details.php
+++ sistema/components/events-details.php
@@ -192,7 +192,12 @@ $concepts = getOtherConcepts($con, $id_event) != "" ? getOtherConcepts($con, $id
     <div class="card">
         <div class="card-content">
             <div class="content has-text-centered">
+                <br>
+                <br>
+                <img class="addButton" style="width: 150px; margin-top: -25px !important;" src="images/logos/logo-bockcao-black.png" alt="logo">
                 <h1 class="title is-4">EVENTO <?= $row['name_event'] ?></h1>
+                <br>
+                <br>
                 <div class="columns">
                     <div class="column is-1"></div>
                     <div class="column is-5 has-text-left">
@@ -401,6 +406,9 @@ $concepts = getOtherConcepts($con, $id_event) != "" ? getOtherConcepts($con, $id
                     </div>
                 </div> <!-- break -->
             </div>
+            <br>
+            <br>
+            <br>
         </div>
     </div>
 </div>
